3f09ee62740c8c18c68cb80a9ba5d5e9152b8b54
Andrew Ha <z3466207@student.unsw.edu.au>
2018-11-18 10:37:47 +1100

    all bugs fixed, only content left to do

--- README.md
+++ README.md
@@ -2,5 +2,3 @@ Features to do:
 progress bar on the right
 
 Bugs to fix:
-animation doesn't work
-skipping pages using side bar makes a white gap on the bottom of the screen
--- scripts/index.js
+++ scripts/index.js
@@ -1,40 +1,46 @@
+/* side bar functions */
 jQuery(function ($) {
 
-    $(".sidebar-dropdown > a").click(function() {
-  $(".sidebar-submenu").slideUp(200);
-  if (
-    $(this)
-      .parent()
-      .hasClass("active")
-  ) {
-    $(".sidebar-dropdown").removeClass("active");
-    $(this)
-      .parent()
-      .removeClass("active");
-  } else {
-    $(".sidebar-dropdown").removeClass("active");
-    $(this)
-      .next(".sidebar-submenu")
-      .slideDown(200);
-    $(this)
-      .parent()
-      .addClass("active");
-  }
+  $(".sidebar-dropdown > a").click(function() {
+
+    $(".sidebar-submenu").slideUp(200);
+
+    if ($(this).parent().hasClass("active")) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this).parent().removeClass("active");
+    } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this).next(".sidebar-submenu").slideDown(200);
+      $(this).parent().addClass("active");
+    }
+  });
+
+  $(".close-sidebar").click(function() {
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function() {
+    $(".page-wrapper").addClass("toggled");
+  });
+
 });
 
-$(".close-sidebar").click(function() {
-  $(".page-wrapper").removeClass("toggled");
-});
-$("#show-sidebar").click(function() {
-  $(".page-wrapper").addClass("toggled");
-});
-});
-
+/* Animating the scroll */
 $(document).ready(function (){
+
+  // $('.scroll').on('click', function() {
+  //   $('html, body').animate({
+  //       scrollTop: $($(this).attr('href')).offset().top
+  //     }, 1000, function() {console.log("Finished animating")}); 
+  // });
+
   $(".scroll").click(function (){
-    console.log("animating scroll");
     $('html, body').animate({
       scrollTop: $($(this).attr('href')).offset().top
-    }, 1000);
+    }, 1000); 
   });
 });
+
+/*no scroll bar */
+/*var parent = document.getElementById('container1');
+var child = document.getElementById('container2');
+child.style.paddingRight = child.offsetWidth - child.clientWidth + "px";*/
